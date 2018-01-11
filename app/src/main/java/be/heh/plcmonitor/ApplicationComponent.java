cc797d9a770d3120c34ca4d471d0516ee19aca1c
Terencio Agozzino <terencio.agozzino@gmail.com>
2018-01-11 10:19:18 +0100

    Add dependencies injections

--- app/src/main/java/be/heh/plcmonitor/ApplicationComponent.java
+++ app/src/main/java/be/heh/plcmonitor/ApplicationComponent.java
@@ -19,10 +19,20 @@ package be.heh.plcmonitor;
 import be.heh.plcmonitor.activity.LoginActivity;
 import be.heh.plcmonitor.activity.MainActivity;
 import be.heh.plcmonitor.activity.RegisterActivity;
-
-import be.heh.plcmonitor.fragment.SettingsFragment;
-import dagger.Component;
+import be.heh.plcmonitor.adapter.PlcsAdapter;
+import be.heh.plcmonitor.adapter.UsersAdapter;
+import be.heh.plcmonitor.fragment.AddPlcFragment;
+import be.heh.plcmonitor.fragment.AddUserFragment;
+import be.heh.plcmonitor.fragment.EditPlcFragment;
+import be.heh.plcmonitor.fragment.EditUserFragment;
+import be.heh.plcmonitor.fragment.PlcsFragment;
+import be.heh.plcmonitor.fragment.UsersFragment;
+import be.heh.plcmonitor.preference.ControlLevelPreference;
+import be.heh.plcmonitor.preference.PillsPreferenceFragment;
+import be.heh.plcmonitor.preference.SettingsPreferenceFragment;
 import be.heh.plcmonitor.database.DatabaseModule;
+
+import dagger.Component;
 
 import javax.inject.Singleton;
 
@@ -37,21 +47,21 @@ import javax.inject.Singleton;
 public interface ApplicationComponent {
 
     /**
+     * Allows LoginActivity to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param loginActivity the login screen that offers login via
+     *                      email/password
+     */
+    void inject(LoginActivity loginActivity);
+
+    /**
      * Allows MainActivity to request dependencies declared by the
      * DatabaseModule class.
      *
      * @param mainActivity the main screen of the application
      */
     void inject(MainActivity mainActivity);
-
-    /**
-     * Allows LoginActivity to request dependencies declared by the
-     * DatabaseModule class.
-     *
-     * @param loginActivity the login screen that offers login via
-     *                      email/password.
-     */
-    void inject(LoginActivity loginActivity);
 
 
     /**
@@ -60,9 +70,98 @@ public interface ApplicationComponent {
      *
      * @param registerActivity the register screen that offers sign up via
      *                         first name, last name, email address, and
-     *                         password.
+     *                         password
      */
     void inject(RegisterActivity registerActivity);
 
-    void inject(SettingsFragment settingsFragment);
+    /**
+     * Allows AddPlcFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param addPlcsFragment the fragment to add a new PLC
+     */
+    void inject(AddPlcFragment addPlcsFragment);
+
+    /**
+     * Allows AddUserFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param addUsersFragment the fragment to add a new user
+     */
+    void inject(AddUserFragment addUsersFragment);
+
+    /**
+     * Allows EditPlcFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param editPlcFragment the fragment to edit a PLC
+     */
+    void inject(EditPlcFragment editPlcFragment);
+
+    /**
+     * Allows EditUserFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param editUserFragment the fragment to edit a user
+     */
+    void inject(EditUserFragment editUserFragment);
+
+    /**
+     * Allows PlcsFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param plcFragment the overview fragment of the registered PLCs
+     *                    for a user
+     */
+    void inject(PlcsFragment plcFragment);
+
+    /**
+     * Allows UsersFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param usersFragment the overview fragment of the registered users
+     */
+    void inject(UsersFragment usersFragment);
+
+    /**
+     * Allows UsersAdapter to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param plcsAdapter the PLCs adapter
+     */
+    void inject(PlcsAdapter plcsAdapter);
+
+    /**
+     * Allows UsersAdapter to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param usersAdapter the users adapter
+     */
+    void inject(UsersAdapter usersAdapter);
+
+    /**
+     * Allows PillsPreferenceFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param pillsPreferenceFragment the preference fragment of the pills PLC.
+     */
+    void inject(PillsPreferenceFragment pillsPreferenceFragment);
+
+    /**
+     * Allows ControlLevelPreference to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param controlLevelPreference the preference fragment of the
+     *                                     regulation PLC.
+     */
+    void inject(ControlLevelPreference controlLevelPreference);
+
+    /**
+     * Allows SettingsPreferenceFragment to request dependencies declared by the
+     * DatabaseModule class.
+     *
+     * @param settingsPreferenceFragment the preference fragment of the user
+     *                                   settings.
+     */
+    void inject(SettingsPreferenceFragment settingsPreferenceFragment);
 }
